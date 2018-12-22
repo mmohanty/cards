242ac91e790d4447e66a7e066115d746fc13a47d
Nick-at-237719576704 <manasmohanty07@gmail.com>
2018-12-22 23:09:09 +0530

    Changing region

--- src/main/java/com/cloudformation/cards/cc/handler/CardTransactionHistoryHandler.java
+++ src/main/java/com/cloudformation/cards/cc/handler/CardTransactionHistoryHandler.java
@@ -22,20 +22,20 @@ public class CardTransactionHistoryHandler implements RequestHandler<Transaction
 		final ClientConfiguration clientConfig = new ClientConfiguration();
 
 		final String tableName = "CardTransaction";
-		final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.standard().withClientConfiguration(clientConfig).withRegion(Regions.AP_SOUTHEAST_1).build();
+		final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.standard().withClientConfiguration(clientConfig).withRegion(Regions.AP_SOUTH_1).build();
 		
 		 // Scan items for transactions with a card-number and transaction date ranging between given start and end date.
         HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
         Condition cardNumberCondition = new Condition()
             .withComparisonOperator(ComparisonOperator.EQ.toString())
             .withAttributeValueList(new AttributeValue().withS(input.getCardNumber()));
-        Condition startDateCondition = new Condition()
+        Condition txnDateCondition = new Condition()
                 .withComparisonOperator(ComparisonOperator.BETWEEN.toString())
                 .withAttributeValueList(new AttributeValue().withS(input.getStartDate()), 
                 		new AttributeValue().withS(input.getEndDate()));
         
         scanFilter.put("cardNumber", cardNumberCondition);
-        scanFilter.put("transactionDate", startDateCondition);
+        scanFilter.put("transactionDate", txnDateCondition);
         
         ScanRequest scanRequest = new ScanRequest(tableName).withScanFilter(scanFilter);
         ScanResult scanResult = dynamoDB.scan(scanRequest);
